4fb9aefebadebf83c7b559877aa97a2dabea60fe
bfuchs18 <bari.fuchs@gmail.com>
2024-03-04 15:36:11 -0500

    prep data for score_pds, subset demo and household datasets

--- R/util_redcap_parent_v1.R
+++ R/util_redcap_parent_v1.R
@@ -4,12 +4,13 @@
 #'
 #'
 #' @param data data from REDCap event parent_visit_1_arm_1
+#' @param v1_date_data dataframe with 2 columns: 'participant_id' and 'v1_date'
 #' @param return_data If return_data is set to TRUE, will return a list including:
 #'  1) clean raw parent 1 datasets
 #'  2) meta-data/.json for each dataset
 #'
 
-util_redcap_parent_v1 <- function(data, return_data = TRUE) {
+util_redcap_parent_v1 <- function(data, v1_date_data, return_data = TRUE) {
 
   #### 1. Set up/initial checks #####
 
@@ -29,20 +30,34 @@ util_redcap_parent_v1 <- function(data, return_data = TRUE) {
   #reduce columns and update names
 
   ## demographics data ####
-  household_data <- data[, grepl('participant_id', names(data)) | grepl('demo', names(data))]
-  household_data <- household_data[, !(names(household_data) %in% c('visit_1_demographics_timestamp',
+  # this data will be split into 2 dataframes:
+    # (1) demo_data: will include child demographic variables that will be include in participants.tsv
+    # (2) household_data: will include variables related to the family and household environemnt
+
+  child_demo_vars <- c('demo_race', 'demo_ethnicity', 'demo_income', 'demo_education_mom', 'demo_child_birthdate')
+
+  demo_data_all <- data[, grepl('participant_id', names(data)) | grepl('demo', names(data))]
+  household_data <- demo_data_all[, !(names(demo_data_all) %in% c('visit_1_demographics_timestamp',
                                                                     'demo_v1_missingcheck',
                                                                     'visit_1_demographics_complete',
                                                                     'parent_household_demographics_questionnaire_timestamp',
                                                                     'demo_missingcheck', 'demo_missingcheck_2', 'demo_missingcheck_3',
-                                                                    'parent_household_demographics_questionnaire_complete'))]
-  # names(household_data)[??] <- 'demo_mom_ed'
-  # determine demo_mom_ed
-  # determine demo_dad_ed
-  # compute age instead of birthday
-  # remove items in demo_data from household_data
+                                                                    'parent_household_demographics_questionnaire_complete',
+                                                                    child_demo_vars))]
+  # subset demo_data
+  demo_data <- demo_data_all[, c('participant_id', child_demo_vars)]
 
-  # demo_data <- household_data[c('participant_id', 'demo_c_dob', 'demo_c_sex', 'demo_race', 'demo_ethnicity', 'demo_child_other_race', 'demo_income', 'demo_mod_ed', 'demo_dad_ed')]
+  # add age to demo_data
+  demo_data <- merge(demo_data, v1_date_data, by = 'participant_id')
+  demo_data[['v1_date']] <- lubridate::as_date(demo_data[['v1_date']])
+  demo_data[['age']] <- lubridate::interval(demo_data[['demo_child_birthdate']], demo_data[['v1_date']])/lubridate::years(1)
+
+  # remove dob and v1 date from demo_data
+  demo_data <- demo_data[, !(names(demo_data) %in% c('demo_child_birthdate','v1_date'))]
+
+  # add sex to demo_data
+  demo_data <- merge(demo_data, data[, c("participant_id", "prs_sex")], by="participant_id")
+  names(demo_data)[names(demo_data) == "prs_sex"] <- "sex"
 
   ## RANK Data (ranking food item questionnaire) ####
   rank_data <- data[, grepl('participant_id', names(data)) | grepl('rank', names(data))]
@@ -53,11 +68,48 @@ util_redcap_parent_v1 <- function(data, return_data = TRUE) {
   puberty_data <- data[, grepl('participant_id', names(data)) | grepl('prs', names(data))]
   names(puberty_data) <- gsub('prs_sex', 'sex', names(puberty_data))
 
-  #re-code sex to match demo_c_sex
-  #  puberty_data[['sex']] <- ifelse(puberty_data[['sex']] == 0, 1, 0)
-  # puberty_data$visit <- 1
+  # scoring prep: update puberty_data to adhere to dataprepr::score_pds requirements:
+    # The data set columns/variables must match the following naming convention: 'sex', 'pds_1', 'pds_2', 'pds_3', 'pds_4m', 'pds_5m', 'pds_4f', 'pds_5fa'.
+    # Values should be: 1 = not started yet, 2 = barely started, 3 = definitely started, 4 = seems complete, and 99 = I Don't Know. For Female ('pds_5fa') question on menarche, the response can be coded as Yes = 1, No = 0.
 
-  #  puberty_scored <- dataprepr::score_pds(puberty_data, score_base = FALSE, respondent = 'parent', male = 0, female = 1, id = 'participant_id')
+  # remap scale values except prs_girls_5
+  value_mapping <- c("0" = 1, "1" = 2, "2" = 3, "3" = 4, "4" = 99)
+  columns_to_remap <- c("prs_boys_1", "prs_boys_2", "prs_boys_3", "prs_boys_4", "prs_boys_5", "prs_boys_6", "prs_girls_1", "prs_girls_2", "prs_girls_3", "prs_girls_4", "prs_girls_6")
+  puberty_data[, columns_to_remap] <- lapply(puberty_data[, columns_to_remap], function(x) value_mapping[as.character(x)])
+
+  # remap prs_girls_5
+  value_mapping_g5 <- c("4" = 99)
+  puberty_data[, "prs_girls_5"] <- ifelse(puberty_data[, "prs_girls_5"] == 4, value_mapping_g5[as.character(4)], puberty_data[, "prs_girls_5"])
+
+  # subset and rename girls variables
+  puberty_data_girls <- subset(puberty_data, sex == 0)
+  puberty_data_girls <- puberty_data_girls[, !grepl('boys', names(puberty_data_girls))]
+  puberty_data_girls <- puberty_data_girls %>% dplyr::rename(
+                                                pds_1 = prs_girls_1,
+                                                pds_2 = prs_girls_2,
+                                                pds_3 = prs_girls_3,
+                                                pds_4f = prs_girls_4,
+                                                pds_5fa = prs_girls_5,
+                                                pds_6 = prs_girls_6,
+                                              )
+
+  # subset and rename boys variables
+  puberty_data_boys <- subset(puberty_data, sex == 1)
+  puberty_data_boys <- puberty_data_boys[, !grepl('girls', names(puberty_data_boys))]
+  puberty_data_boys <- puberty_data_boys %>% dplyr::rename(
+                                                pds_1 = prs_boys_1,
+                                                pds_2 = prs_boys_2,
+                                                pds_3 = prs_boys_3,
+                                                pds_4m = prs_boys_4,
+                                                pds_5m = prs_boys_5,
+                                                pds_6 = prs_boys_6,
+                                              )
+
+  # bind girls and boys dfs -- dplyr::bind_rows fills missing values with NA where columns don't match.
+  puberty_data_for_scoring <- dplyr::bind_rows(puberty_data_girls, puberty_data_boys)
+
+  # this requires debugging:
+  #puberty_scored <- dataprepr::score_pds(puberty_data_for_scoring, score_base = FALSE, respondent = 'parent', male = "1", female = "0", id = 'participant_id')
 
   ## CFQ Data ####
   cfq_data <- data[, grepl('participant_id', names(data)) | grepl('cfq', names(data))]
@@ -79,7 +131,7 @@ util_redcap_parent_v1 <- function(data, return_data = TRUE) {
   chaos_data <- chaos_data[, !(names(chaos_data) %in% c('confusion_hubbub_and_order_scale_chaos_timestamp', 'confusion_hubbub_and_order_scale_chaos_complete'))]
   # need to develop score script
 
-  ## PSS Data  ####
+  ## PSS Data  (percieved stress scale) ####
   pss_data <- data[, grepl('participant_id', names(data)) | grepl('pss', names(data))]
   # need to develop score script
   #pss_scored <-
@@ -93,14 +145,17 @@ util_redcap_parent_v1 <- function(data, return_data = TRUE) {
   ## compile and return data ####
   if (isTRUE(return_data)){
     return(list(
-      #demo_data = list(data = demo_data),
+      demo_data = list(data = demo_data),
       household_data = household_data,
       rank_data = rank_data,
+#      puberty_data = puberty_scored, # score script needs to be debugged and applied
       puberty_data = puberty_data,
       cfq_data = cfq_scored,
       cebq_data = cebq_scored,
       efcr_data = efcr_scored,
+#      chaos_data = chaos_scored, #score script to be developed
       chaos_data = chaos_data,
+#      pss_data = pss_scored, # score script to be developed
       pss_data = pss_data,
       lbc_data = lbc_scored))
   }
