c402399bac1315735c8d93a0f1ec6b3e463853dc
bfuchs18 <bari.fuchs@gmail.com>
2024-03-08 12:35:41 -0500

    add very large if/else statement to prep fhfi vars with a__0, a__1, etc

--- R/util_redcap_parent_v4.R
+++ R/util_redcap_parent_v4.R
@@ -28,8 +28,8 @@ util_redcap_parent_v4 <- function(data, return_data = TRUE) {
   #reduce columns and update names
 
   ## Visit Data ####
-  v4_data <- data[, grepl('participant_id', names(data)) | grepl('update', names(data))]
-  v4_data <- v4_data[, !(names(v4_data) %in% c('participant_update_form_timestamp', 'participant_update_form_complete', 'update_form_contact'))]
+  visit_data_parent <- data[, grepl('participant_id', names(data)) | grepl('update', names(data))]
+  visit_data_parent <- visit_data_parent[, !(names(visit_data_parent) %in% c('participant_update_form_timestamp', 'participant_update_form_complete', 'update_form_contact'))]
   # notes: update_form_visit_number -- looks to be the session number attended, not protocol visit number
   # add column to specify v4 data?
 
@@ -59,9 +59,116 @@ util_redcap_parent_v4 <- function(data, return_data = TRUE) {
   audit_data <- audit_data[, !(names(audit_data) %in% c('audit_missingcheck'))]
   audit_scored <- dataprepr::score_audit(audit_data, id = 'participant_id')
 
-  ## FHFI Data ####
+  ## Fulkerson HFI Data ####
   # this refers to the fulkerson_home_food_inventory
   fhfi_data <- data[, grepl('participant_id', names(data)) | grepl('fhfi', names(data))]
+  names(fhfi_data) <- gsub('fhfi', 'hfi', names(fhfi_data))
+
+  # fhfi_data <- util_format_fhfi(fhfi_data) # move everything into a separate function
+
+  # vegetable:
+    # a___0 =  _fresh
+    # a___1 =  _can
+    # a___2 =  _frozen
+  # fruit
+    # a___0 =  _fresh
+    # a___1 =  _can
+    # a___2 =  _frozen
+    # a___3 =  _dried
+  # bread
+    # a___0 =  _fresh
+    # a___1 =  _frozen
+  # prepared dessert
+    # a___0 =  _storebought
+    # a___1 =  _homemade
+
+  for (col in names(fhfi_data)) {
+    if (grepl("vegetable", col) == TRUE) {
+      if (grepl("a___0", col) == TRUE) {
+        names(fhfi_data)[which(names(fhfi_data) == col)] <-
+          sub("a___0", "_fresh", col)
+      } else {
+        if (grepl("a___1", col) == TRUE) {
+          names(fhfi_data)[which(names(fhfi_data) == col)] <-
+            sub("a___1", "_can", col)
+        } else {
+          if (grepl("a___2", col) == TRUE) {
+            names(fhfi_data)[which(names(fhfi_data) == col)] <-
+              sub("a___2", "_frozen", col)
+          }
+        }
+      }
+    } else {
+      if (grepl("fruit", col) == TRUE) {
+        if (grepl("a___0", col) == TRUE) {
+          names(fhfi_data)[which(names(fhfi_data) == col)] <-
+            sub("a___0", "_fresh", col)
+        } else {
+          if (grepl("a___1", col) == TRUE) {
+            names(fhfi_data)[which(names(fhfi_data) == col)] <-
+              sub("a___1", "_can", col)
+          } else {
+            if (grepl("a___2", col) == TRUE) {
+              names(fhfi_data)[which(names(fhfi_data) == col)] <-
+                sub("a___2", "_frozen", col)
+            } else {
+              if (grepl("a___3", col) == TRUE) {
+                names(fhfi_data)[which(names(fhfi_data) == col)] <-
+                  sub("a___3", "_dried", col)
+              }
+            }
+          }
+        }
+      } else {
+        if (grepl("bread", col) == TRUE) {
+          if (grepl("a___0", col) == TRUE) {
+            names(fhfi_data)[which(names(fhfi_data) == col)] <-
+              sub("a___0", "_fresh", col)
+          } else {
+            if (grepl("a___1", col) == TRUE) {
+              names(fhfi_data)[which(names(fhfi_data) == col)] <-
+                sub("a___1", "_frozen", col)
+            }
+          }
+        } else {
+          if (grepl("prepared_dessert", col) == TRUE) {
+            if (grepl("a___0", col) == TRUE) {
+              names(fhfi_data)[which(names(fhfi_data) == col)] <-
+                sub("a___0", "_storebought", col)
+            } else {
+              if (grepl("a___1", col) == TRUE) {
+                names(fhfi_data)[which(names(fhfi_data) == col)] <-
+                  sub("a___1", "_homemade", col)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  # Convert numbers to letters
+  for (col in names(fhfi_data)) {
+
+    # extract after last _
+    extracted <- sub(".*_", "", col)
+
+      # if extracted can be converted to numeric
+      if (varhandle::check.numeric(extracted) == TRUE ) {
+        extracted_num = as.numeric(extracted)
+        letter_replacement = letters[extracted_num]
+
+        # replace extracted with letter_replacement
+        # note: this wont work for something like hfi_vegetable_1a___1 (the first 1 will be replaced)- need to deal with those first
+        names(fhfi_data)[which(names(fhfi_data) == col)] <- sub(extracted, letter_replacement, col)
+      }
+  }
+
+  # fix category names
+  # _dessert to _frozen_dessert
+  # _accessible to _visible
+  #
+
   #fhfi_scored <- dataprepr::score_hfi(hfi_data, score_base = TRUE, id = 'participant_id') # need to make sure data is ready to go into this
 
   ## CFPQ Data ####
@@ -71,7 +178,7 @@ util_redcap_parent_v4 <- function(data, return_data = TRUE) {
   ## compile and return data ####
   if (isTRUE(return_data)){
     return(list(
-      v4_data = v4_data,
+      visit_data_parent = visit_data_parent,
       # hfias_data = hfias_scored,
       hfssm_data = hfssm_scored,
       pmum_data = pmum_data,
