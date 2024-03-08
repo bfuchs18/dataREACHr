ee94ef3a86cccdcd892dd4404bc98bc4861c1dc1
bfuchs18 <bari.fuchs@gmail.com>
2024-03-08 12:41:36 -0500

    move fhfi data prep to separate function

---
+++ R/util_format_fhfi_data.R
@@ -0,0 +1,114 @@
+#' util_format_fhfi_data: Prepare Fulkerson HFI data for scoring (called within util_redcap_parent_v4.R)
+#'
+#' This function prepares Fulkerson HFI data for scoring with dataprepr::score_hfi()
+#'
+#'
+#' @param data Fulkerson HFI extracted from data from REDCap event parent_visit_4_arm_1'
+#'
+util_format_fhfi_data <- function(data) {
+
+  # Fix type labels
+
+  # vegetable:
+    # a___0 =  _fresh
+    # a___1 =  _can
+    # a___2 =  _frozen
+  # fruit
+    # a___0 =  _fresh
+    # a___1 =  _can
+    # a___2 =  _frozen
+    # a___3 =  _dried
+  # bread
+    # a___0 =  _fresh
+    # a___1 =  _frozen
+  # prepared dessert
+    # a___0 =  _storebought
+    # a___1 =  _homemade
+
+  for (col in names(fhfi_data)) {
+    if (grepl("vegetable", col) == TRUE) {
+      if (grepl("a___0", col) == TRUE) {
+        names(fhfi_data)[which(names(fhfi_data) == col)] <-
+          sub("a___0", "_fresh", col)
+      } else {
+        if (grepl("a___1", col) == TRUE) {
+          names(fhfi_data)[which(names(fhfi_data) == col)] <-
+            sub("a___1", "_can", col)
+        } else {
+          if (grepl("a___2", col) == TRUE) {
+            names(fhfi_data)[which(names(fhfi_data) == col)] <-
+              sub("a___2", "_frozen", col)
+          }
+        }
+      }
+    } else {
+      if (grepl("fruit", col) == TRUE) {
+        if (grepl("a___0", col) == TRUE) {
+          names(fhfi_data)[which(names(fhfi_data) == col)] <-
+            sub("a___0", "_fresh", col)
+        } else {
+          if (grepl("a___1", col) == TRUE) {
+            names(fhfi_data)[which(names(fhfi_data) == col)] <-
+              sub("a___1", "_can", col)
+          } else {
+            if (grepl("a___2", col) == TRUE) {
+              names(fhfi_data)[which(names(fhfi_data) == col)] <-
+                sub("a___2", "_frozen", col)
+            } else {
+              if (grepl("a___3", col) == TRUE) {
+                names(fhfi_data)[which(names(fhfi_data) == col)] <-
+                  sub("a___3", "_dried", col)
+              }
+            }
+          }
+        }
+      } else {
+        if (grepl("bread", col) == TRUE) {
+          if (grepl("a___0", col) == TRUE) {
+            names(fhfi_data)[which(names(fhfi_data) == col)] <-
+              sub("a___0", "_fresh", col)
+          } else {
+            if (grepl("a___1", col) == TRUE) {
+              names(fhfi_data)[which(names(fhfi_data) == col)] <-
+                sub("a___1", "_frozen", col)
+            }
+          }
+        } else {
+          if (grepl("prepared_dessert", col) == TRUE) {
+            if (grepl("a___0", col) == TRUE) {
+              names(fhfi_data)[which(names(fhfi_data) == col)] <-
+                sub("a___0", "_storebought", col)
+            } else {
+              if (grepl("a___1", col) == TRUE) {
+                names(fhfi_data)[which(names(fhfi_data) == col)] <-
+                  sub("a___1", "_homemade", col)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  # Convert numbers to letters
+  for (col in names(fhfi_data)) {
+
+    # extract after last _
+    extracted <- sub(".*_", "", col)
+
+    # if extracted can be converted to numeric
+    if (varhandle::check.numeric(extracted) == TRUE ) {
+      extracted_num = as.numeric(extracted)
+      letter_replacement = letters[extracted_num]
+
+      # replace extracted with letter_replacement
+      # note: this wont work for something like hfi_vegetable_1a___1 (the first 1 will be replaced)- need to deal with those first
+      names(fhfi_data)[which(names(fhfi_data) == col)] <- sub(extracted, letter_replacement, col)
+    }
+  }
+
+  # fix category names
+  # _dessert to _frozen_dessert
+  # _accessible to _visible
+  #
+}
