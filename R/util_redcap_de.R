10eb3e0aabe9d9010104f37ffc84a92de9ba5fd6
bfuchs18 <bari.fuchs@gmail.com>
2024-03-15 15:33:15 -0400

    add framework for calculating child bmi variables

--- R/util_redcap_de.R
+++ R/util_redcap_de.R
@@ -7,7 +7,7 @@
 #' @param return_data If return_data is set to TRUE, will return a list including: mri_visit_data, tictoc_data, wasi_data, dexa_data, anthro_data (wide and long), intake_data (wide and long)
 #'
 
-util_redcap_de <- function(data, return_data = TRUE) {
+util_redcap_de <- function(data, agesex_data, return_data = TRUE) {
 
   #### 1. Set up/initial checks #####
 
@@ -45,20 +45,51 @@ util_redcap_de <- function(data, return_data = TRUE) {
   wasi_data <- data[, grep("participant_id|wasi.*score$|wasi_percentile", names(data))]
 
   ## Anthropometrics data ####
-  anthro_data <- data[, grep("participant_id|parent_height|child_height|parent_weight|child_weight|bmi", names(data))]
+  anthro_data <- data[, grep("participant_id|parent_height|child_height|parent_weight|child_weight|child_average_weight|bmi", names(data))]
   anthro_data <- anthro_data[, -grep("complete|self_report", names(anthro_data))] #self-report will be merged from household questionnaire -- this is automatically taken from there (not entered data)
 
-  colnames(anthro_data) <- gsub("child_bmi_v1", "child_bmi", colnames(anthro_data))
+  # rename parent variables with parent1
   colnames(anthro_data) <- gsub("parent_", "parent1_", colnames(anthro_data))
 
-  # child bmi and percentile values were determined using the online form and entered into redcap -- recalculate?
+  # remove child bmi values that were entered into redcao
+  anthro_data <- anthro_data[, -grep("child_bmi", names(anthro_data))]
+
+  # make all vales numeric except column 1 (participant_id)
+  anthro_data <- dplyr::mutate_at(anthro_data, -1, function(x) as.numeric(as.character(x)))
+
+  # add sex and age to calculate BMI derivatives
+  anthro_data <- merge(anthro_data, agesex_data[c("participant_id", "sex", "v1_age", "v5_age")], by = "participant_id", all = TRUE)
+
+  # calculate child bmi values -- need to debug once there is data entered
+
+  # ## visit 1 BMI
+  # anthro_data$child_bmi_v1 <-
+  #   ifelse(is.na(anthro_data[["child_height_average_v1"]]) | is.na(anthro_data[["child_average_weight_v1"]]), NA,
+  #          round(anthro_data[["child_average_weight_v1"]] / ((
+  #            anthro_data[["child_height_average_v1"]] / 100
+  #          ) ^ 2),
+  #          digits = 2))
+  #
+  # anthro_data$child_bmi_z_v1 <- round(childsds::sds(value = anthro_data[["child_bmi_v1"]], age = anthro_data[["v1_age"]], sex = anthro_data[['sex']], item = "bmi", ref = childsds::cdc.ref, type = "SDS", male = 1, female = 0), digits = 2)
+  # anthro_data$child_bmi_p_v1 <- round((childsds::sds(value = anthro_data[["child_bmi_v1"]], age = anthro_data[["v1_age"]], sex = anthro_data[['sex']], item = "bmi", ref = childsds::cdc.ref, type = "perc", male = 1, female = 0)) * 100, digits = 2)
+  #
+  # ## visit 5 BMI
+  # anthro_data$child_bmi_v5 <-
+  #   ifelse(is.na(anthro_data[["child_height_average_v5"]]) | is.na(anthro_data[["child_average_weight_v5"]]), NA,
+  #          round(anthro_data[["child_average_weight_v5"]] / ((
+  #            anthro_data[["child_height_average_v5"]] / 100
+  #          ) ^ 2),
+  #          digits = 2))
+  #
+  # anthro_data$child_bmi_z_v5 <- round(childsds::sds(value = anthro_data[["child_bmi_v5"]], age = anthro_data[["v5_age"]], sex = anthro_data[['sex']], item = "bmi", ref = childsds::cdc.ref, type = "SDS", male = 1, female = 0), digits = 2)
+  # anthro_data$child_bmi_p_v5 <- round((childsds::sds(value = anthro_data[["child_bmi_v5"]], age = anthro_data[["v5_age"]], sex = anthro_data[['sex']], item = "bmi", ref = childsds::cdc.ref, type = "perc", male = 1, female = 0)) * 100, digits = 2)
 
   # separate visit data
   anthro_v1_data <- anthro_data[, grep("participant_id|v1", names(anthro_data))]
-  colnames(anthro_v1_data) <- gsub("_v1", "", colnames(anthro_v1_data)) # Remove "v1_" from column names
+  colnames(anthro_v1_data) <- gsub("_v1|v1_", "", colnames(anthro_v1_data)) # Remove "v1_" or "_v1" from column names
 
   anthro_v5_data <- anthro_data[, grep("participant_id|v5", names(anthro_data))]
-  colnames(anthro_v5_data) <- gsub("_v5", "", colnames(anthro_v5_data)) # Remove "v5_" from column names
+  colnames(anthro_v5_data) <- gsub("_v5|v5_", "", colnames(anthro_v5_data)) # Remove "v5_" or "_v5" from column names
 
   # make all vales numeric
   anthro_v1_data <- dplyr::mutate_all(anthro_v1_data, function(x) as.numeric(as.character(x)))
