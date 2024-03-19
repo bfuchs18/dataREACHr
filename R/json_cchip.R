4fcb0f11cfc20a1d6d1b65d439a75eded4553bda
bfuchs18 <bari.fuchs@gmail.com>
2024-03-19 17:30:25 -0400

    init commit json functions

---
+++ R/json_cchip.R
@@ -0,0 +1,82 @@
+#' json_cchip: Generates a json file for the Community Childhood Hunger Identification Project
+#'
+#' This function generates a json file for the scored Community Childhood Hunger Identification Project and raw participant responses.
+#' This function provides accurate json files ONLY if data is processed using score_cchip function in dataprepr and is only accurate for data collected in Study REACH
+#'
+#' @return A json file documenting the raw inputs and scored values for the Community Childhood Hunger Identification Project
+#'
+#'
+#' @export
+
+json_cchip <- function() {
+
+  cchip_list <- list(
+    'MeasurementToolMetadata' = list(
+      Description = 'Community Childhood Hunger Identification Project',
+      Reference = 'Wehler CA, Scott RI, Anderson JJ. The community childhood hunger identification project: A model of domestic hunger—Demonstration project in Seattle, Washington. Journal of Nutrition Education. 1992;24(1):29S-35S. doi:10.1016/S0022-3182(12)80135-X',
+      TermURL = 'https://www.sciencedirect.com/science/article/abs/pii/S002231821280135X'),
+    participant_id = list( Description = 'participant id number'),
+    cchip1 = list( Description = 'Thinking about the past 12 months, did your household ever run out of money to buy food to make a meal?',
+                  Levels = list ('0' = 'No',
+                                 '1' = 'Yes')),
+    cchip2 = list( Description = 'Thinking about the past 12 months, during how many months did your household run out of money to buy food to make a meal? (free response)'),
+    cchip3 = list( Description = 'When your household runs out of money to make a meal, how many days per month are you usually without food? (free response)'),
+    cchip4 = list( Description = 'Thinking about the past 30 days, how many days was your household out of money to buy food to make a meal? (free response)'),
+    cchip5 = list( Description = 'Thinking about the past 12 months, did your household ever rely on a limited number of foods to feed your children because you were running out of money to buy food for a meal?',
+                   Levels = list ('0' = 'No',
+                                  '1' = 'Yes')),
+    cchip6 = list( Description = 'Thinking about the past 12 months, during how many months did your household rely on a limited number of foods to feed your children because you were running out of money to buy food for a meal? (free response)'),
+    cchip7 = list( Description = 'When your household runs out of money to buy food for a meal, how many days per month do you usually rely on a limited number of foods to feed your children? (free response)'),
+    cchip8 = list( Description = 'Thinking about the past 30 days, how many days did your household rely on a limited number of foods to feed your children because you were running out of money to buy food for a meal? (free response)'),
+    cchip9 = list( Description = 'Thinking about the past 12 months, did you or adult members of your household ever eat less than you felt you should because there was not enough money for food?',
+                   Levels = list ('0' = 'No',
+                                  '1' = 'Yes')),
+    cchip10 = list( Description = 'Thinking about the past 12 months, during how many months did you or adult members of your household ever eat less than you felt you should because there was not enough money for food? (free response)'),
+    cchip11 = list( Description = 'Thinking about the past 30 days, how many days did you or adult members of your household eat less than you felt you should because there was not enough money for food? (free response)'),
+    cchip12 = list( Description = 'Thinking about the past 30 days, how many days did you or adult members of your household eat less than you felt you should because there was not enough money for food? (free response)'),
+    cchip13 = list( Description = 'Thinking about the past 12 months, did you or adult members of your household ever cut the size of meals or skip meals because there was not enough money for food?',
+                    Levels = list ('0' = 'No',
+                                   '1' = 'Yes')),
+    cchip14 = list( Description = 'Thinking about the past 12 months, during how many months did you or adult members of your household cut the size of meals or skip meals because there was not enough money for food? (free response)'),
+    cchip15 = list( Description = 'When your household does not have enough money for food, how many days per month do you or adult members of your household usually cut the size of meals or skip meals? (free response)'),
+    cchip16 = list( Description = 'Thinking about the past 30 days, how many days did you or adult members of your household cut the size of meals or skip meals because there was not enough money for food? (free response)'),
+    cchip17 = list( Description = 'Thinking about the past 12 months, did your children ever say they were hungry because there wasn\'t enough food in the house?',
+                    Levels = list ('0' = 'No',
+                                   '1' = 'Yes')),
+    cchip18 = list( Description = 'Thinking about the past 12 months, during how many months did your children say they were hungry because there wasn\'t enough food in the house? (free response)'),
+    cchip19 = list( Description = 'When there isn\'t enough food in the house, how many days per month do your children usually say they are hungry? (free response)'),
+    cchip20 = list( Description = 'Thinking about the past 30 days, how many days did your children say they were hungry because there wasn\'t enough food in the house? (free response)'),
+    cchip21 = list( Description = 'Thinking about the past 12 months, did your children ever eat less than you felt they should because there was not enough money for food?',
+                    Levels = list ('0' = 'No',
+                                   '1' = 'Yes')),
+    cchip22 = list( Description = 'Thinking about the past 12 months, during how many months did your children eat less than you felt they should because there was not enough money for food? (free response)'),
+    cchip23 = list( Description = 'When there isn\'t enough money for food, how many days per month do your children usually eat less than you feel they should? (free response)'),
+    cchip24 = list( Description = 'Thinking about the past 30 days, how many days did your children eat less than you felt they should because there was not enough money for food? (free response)'),
+    cchip25 = list( Description = 'Thinking about the past 12 months, did your children ever go to bed hungry because there was not enough money for food?',
+                    Levels = list ('0' = 'No',
+                                   '1' = 'Yes')),
+    cchip26 = list( Description = 'Thinking about the past 12 months, during how many months did your children ever go to bed hungry because there was not enough money for food? (free response)'),
+    cchip27 = list( Description = 'When there isn\'t enough money for food, how many days per month do your children usually go to bed hungry? (free response)'),
+    cchip28 = list( Description = 'Thinking about the past 30 days, how many days did your children go to bed hungry because there was not enough money for food? (free response)'),
+    cchip29 = list( Description = 'Thinking about the past 12 months, did you ever cut the size of your children\'s meals, or did they ever skip meals, because there was not enough money for food?',
+                    Levels = list ('0' = 'No',
+                                   '1' = 'Yes')),
+    cchip30 = list( Description = 'Thinking about the past 12 months, during how many months did you ever cut the size of your children\'s meals, or did they ever skip meals, because there was not enough money for food? (free response)'),
+    cchip31 = list( Description = 'When there isn\'t enough money for food, how many days per month do you usually cut the size of your children\'s meals or do they skip meals? (free response)'),
+    cchip32 = list( Description = 'Thinking about the past 30 days, how many days did you cut the size of your children\'s meals, or did they skip meals, because there was not enough money for food? (free response)'),
+    cchip_total = list( Description = 'Food Insecurity Score',
+                       Derivative = TRUE),
+    cchip_category = list( Description = 'Food Insecurity Category',
+                        Derivative = TRUE))
+
+  # convert formatting to JSON
+  cchip_json <- RJSONIO::toJSON(cchip_list, pretty = TRUE)
+
+  # double check
+  if (isFALSE(RJSONIO::isValidJSON(cchip_json, asText = TRUE))){
+    print('cchip JSON file may be invalid')
+  }
+
+  return(cchip_json)
+
+}
---
+++ R/json_sic.R
@@ -0,0 +1,145 @@
+#' json_sic: Generates a json file for the Stress in Children Questionnaire
+#'
+#' This function generates a json file for the scored Stress in Children Questionnaire and raw participant responses.
+#' This function provides accurate json files ONLY if data is processed using score_sic function in dataprepr and is only accurate for data collected in Study REACH
+#'
+#' @return A json file documenting the raw inputs and scored values for the Stress in Children Questionnaire
+#'
+#'
+#' @export
+
+json_sic <- function() {
+
+  sic_list <- list(
+    'MeasurementToolMetadata' = list(
+      Description = 'Stress in Children Questionnaire. Participants were provided the following instructions: "The following questions ask you about your feelings and thoughts. For each question, you will be asked to provide a response that best fits."',
+      Reference = 'Osika W, Friberg P, Wahrborg P. A new short self-rating questionnaire to assess stress in children. Int J Behav Med. 2007;14(2):108-117. doi:10.1007/BF03004176',
+      TermURL = 'https://pubmed.ncbi.nlm.nih.gov/17926439/'),
+    participant_id = list( Description = 'participant id number'),
+    sic1 = list( Description = 'I get angry',
+                  Levels = list ('0' = '1 = Never',
+                                 '1' = '2 = Sometimes',
+                                 '2' = '3 = Often',
+                                 '3' = '4 = Very Often')),
+    sic2 = list( Description = 'I get headaches',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic3 = list( Description = 'I like going to school',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic4 = list( Description = 'I feel calm and happy',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic5 = list( Description = 'I get stomach pains',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic6 = list( Description = 'I feel lonely',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic7 = list( Description = 'I get sad',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic8 = list( Description = 'I like to be at school',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic9 = list( Description = 'The other kids tease me',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic10 = list( Description = 'I fall asleep easily at night',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic11 = list( Description = 'I feel calm',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic12 = list( Description = 'Things work out as I have planned',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic13 = list( Description = 'I feel happy',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic14 = list( Description = 'When I am happy I show it',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic15 = list( Description = 'Sometimes I do not reach the goal I have planned for',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic16 = list( Description = 'When I have a hard time it helps being with my friends',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic17 = list( Description = 'When I am sad I show it',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic18 = list( Description = 'Sometimes I can\'t manage with the things I have to do',
+                 Levels = list ('0' = '1 = Never',
+                                '1' = '2 = Sometimes',
+                                '2' = '3 = Often',
+                                '3' = '4 = Very Often')),
+    sic19 = list( Description = 'When I have a hard time there is an adult to talk to',
+                  Levels = list ('0' = '1 = Never',
+                                 '1' = '2 = Sometimes',
+                                 '2' = '3 = Often',
+                                 '3' = '4 = Very Often')),
+    sic20 = list( Description = 'If anyone teases me I will protest',
+                  Levels = list ('0' = '1 = Never',
+                                 '1' = '2 = Sometimes',
+                                 '2' = '3 = Often',
+                                 '3' = '4 = Very Often')),
+    sic21 = list( Description = 'It is easy to concentrate during lessons at school',
+                  Levels = list ('0' = '1 = Never',
+                                 '1' = '2 = Sometimes',
+                                 '2' = '3 = Often',
+                                 '3' = '4 = Very Often')),
+    sic_lackwellbeing = list( Description = 'Lack of Well being',
+                       Derivative = TRUE),
+    sic_distress = list( Description = 'Distress',
+                      Derivative = TRUE),
+    sic_lacksocialsupport = list( Description = 'Lack of Social support',
+                      Derivative = TRUE),
+    sic_grand_mean = list( Description = 'Stress in Children Grand Mean',
+                              Derivative = TRUE)
+    )
+
+
+  # convert formatting to JSON
+  sic_json <- RJSONIO::toJSON(sic_list, pretty = TRUE)
+
+  # double check
+  if (isFALSE(RJSONIO::isValidJSON(sic_json, asText = TRUE))){
+    print('sic JSON file may be invalid')
+  }
+
+  return(sic_json)
+
+}
