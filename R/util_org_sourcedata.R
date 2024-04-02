193c4761804604e083a5ee37f218fa26bb948bf5
bfuchs18 <bari.fuchs@gmail.com>
2024-04-02 19:03:27 -0400

    define sub function to copy data

--- R/util_org_sourcedata.R
+++ R/util_org_sourcedata.R
@@ -21,7 +21,7 @@ util_org_sourcedata <- function(base_wd, overwrite = FALSE) {
 
   # base_wd = "/Users/baf44/projects/Keller_Marketing/ParticipantData/"
 
-  #### 1. Set up/initial checks #####
+  #### Set up/initial checks #####
 
   # check that audit_data exist and is a data.frame
   data_arg <- methods::hasArg(base_wd)
@@ -44,55 +44,8 @@ util_org_sourcedata <- function(base_wd, overwrite = FALSE) {
     print('util_task_foodview_orgraw.R has not been thoroughly tested on Windows systems, may have data_path errors. Contact Bari at baf44@psu.edu if there are errors')
   }
 
-
-  #### FoodView task ####
-  foodview_dir <- paste0(base_wd, slash, 'untouchedRaw', slash, 'foodview_task')
-
-  foodview_files <- list.files(foodview_dir, pattern = 'foodview', full.names = TRUE)
-
-  for (file in foodview_files) {
-
-    # extract file name
-    filename <- basename(file)
-
-    # extract subject from file name
-    temp <- sub('.*-', '', file) # extract characters after final "-" (replace everything up to and including the last occurrence of a hyphen in the string with "")
-    sub_num <- sub('.txt', '', temp) # extract sub number (replace .txt with "")
-    sub_str <- sprintf("sub-%03d", as.numeric(sub_num))
-
-    # set sourcedata directory for task files
-    sub_task_source_dir <- paste0(base_wd, slash, 'bids', slash, 'sourcedata', slash, sub_str, slash, "ses-1", slash, "beh")
-
-    # set sourcedata file
-    sub_task_source_file <- paste0(sub_task_source_dir, slash, filename)
-
-    # create sub_task_source_dir if it doesnt exist
-    if (!dir.exists(sub_task_source_dir)) {
-      dir.create(sub_task_source_dir, recursive = TRUE)
-    }
-
-    # copy file into sub_task_source_dir if it doesn't exist or overwrite = TRUE
-    if (!file.exists(sub_task_source_file) | isTRUE(overwrite)) {
-
-        file.copy(from = file, to = sub_task_source_file)
-    }
-  }
-
-  #### Stop Signal Task ####
-  # to do: reduce reptitive code between sst and food view
-  sst_dir <- paste0(base_wd, slash, 'untouchedRaw', slash, 'sst')
-
-  sst_files <- list.files(sst_dir, pattern = 'stop', full.names = TRUE)
-
-  for (file in sst_files) {
-
-    # extract file name
-    filename <- basename(file)
-
-    # extract subject from file name
-    temp <- sub('.*-', '', file) # extract characters after final "-" (replace everything up to and including the last occurrence of a hyphen in the string with "")
-    sub_num <- sub('.txt', '', temp) # extract sub number (replace .txt with "")
-    sub_str <- sprintf("sub-%03d", as.numeric(sub_num))
+  #### Define copy_to_source() ####
+  copy_to_source <- function(filename, sub_str, overwrite = overwrite) {
 
     # set sourcedata directory for task files
     sub_task_source_dir <- paste0(base_wd, slash, 'bids', slash, 'sourcedata', slash, sub_str, slash, "ses-1", slash, "beh")
@@ -109,7 +62,46 @@ util_org_sourcedata <- function(base_wd, overwrite = FALSE) {
     if (!file.exists(sub_task_source_file) | isTRUE(overwrite)) {
 
       file.copy(from = file, to = sub_task_source_file)
+      print("copying to sourcedata")
     }
+  }
+
+  #### FoodView task ####
+
+  foodview_dir <- paste0(base_wd, slash, 'untouchedRaw', slash, 'foodview_task')
+  foodview_files <- list.files(foodview_dir, pattern = 'foodview', full.names = TRUE)
+
+  for (file in foodview_files) {
+
+    # extract file name
+    filename <- basename(file)
+
+    # extract subject from file name
+    temp <- sub('.*-', '', file) # extract characters after final "-" (replace everything up to and including the last occurrence of a hyphen in the string with "")
+    sub_num <- sub('.txt', '', temp) # extract sub number (replace .txt with "")
+    sub_str <- sprintf("sub-%03d", as.numeric(sub_num))
+
+    # copy to sourcedata
+    copy_to_source(filename, sub_str, overwrite)
+  }
+
+  #### Stop Signal Task ####
+
+  sst_dir <- paste0(base_wd, slash, 'untouchedRaw', slash, 'sst')
+  sst_files <- list.files(sst_dir, pattern = 'stop', full.names = TRUE)
+
+  for (file in sst_files) {
+
+    # extract file name
+    filename <- basename(file)
+
+    # extract subject from file name
+    temp <- sub('.*-', '', file) # extract characters after final "-" (replace everything up to and including the last occurrence of a hyphen in the string with "")
+    sub_num <- sub('.txt', '', temp) # extract sub number (replace .txt with "")
+    sub_str <- sprintf("sub-%03d", as.numeric(sub_num))
+
+    # copy to sourcedata
+    copy_to_source(filename, sub_str, overwrite)
   }
 }
 
