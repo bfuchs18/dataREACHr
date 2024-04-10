fc68a28aecb5219faf47f029eefeb851274b2493
bfuchs18 <bari.fuchs@gmail.com>
2024-04-10 11:59:51 -0400

    update variable name and descriptions

--- R/json_anthro.R
+++ R/json_anthro.R
@@ -21,13 +21,13 @@ json_anthro <- function() {
                               Unit = "kg"),
     child_weight_2_kg = list( Description = 'child weight measurement 2',
                               Unit = "kg"),
-    child_height_average = list( Description = ' v',
+    child_height_average = list( Description = 'average of child_height_1_cm and child_height_2_cm',
                                  Unit = "cm",
                                  Derivative = TRUE),
     child_average_weight = list( Description = 'average of child_weight_1_kg and child_weight_2_kg',
                                  Unit = "kg",
                                  Derivative = TRUE),
-    parent1_height_sex = list( Description = 'sex of parent with measured height and weight'),
+    parent1_sex = list( Description = 'sex of parent with measured height and weight'),
     parent1_height_1_cm = list( Description = 'parent height measurement 1',
                                 Unit = "cm"),
     parent1_height_2_cm = list( Description = 'parent height measurement 2',
@@ -44,12 +44,12 @@ json_anthro <- function() {
                                       Derivative = TRUE),
     parent1_bmi_redcap_calc = list( Description = '',
                                     Derivative = TRUE),
-    child_bmi = list( Description = '',
-                                    Derivative = TRUE),
-    child_bmi_p = list( Description = '',
-                                 Derivative = TRUE),
-    child_bmi_z = list( Description = '',
-                                 Derivative = TRUE)
+    child_bmi = list( Description = 'Child BMI',
+                      Derivative = TRUE),
+    child_bmi_p = list( Description = 'Child BMI-for-age percentile based on the LMS Parameters for the Centers for Disease Control and Prevention 2000 Growth Charts. Calculated in R using childsds package',
+                        Derivative = TRUE),
+    child_bmi_z = list( Description = 'Child BMI-for-age z-score baesd on the LMS Parameters for the Centers for Disease Control and Prevention 2000 Growth Charts. Calculated in R using childsds package',
+                        Derivative = TRUE)
   )
 
   # convert formatting to JSON
