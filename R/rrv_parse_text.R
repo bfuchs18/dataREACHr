69a5089c9a5019c15bba272f9da5a8b784559002
bfuchs18 <bari.fuchs@gmail.com>
2024-04-23 18:09:08 -0400

    create summary dataframe for summary.csv and export csv files

--- R/rrv_parse_text.R
+++ R/rrv_parse_text.R
@@ -1,20 +1,19 @@
 #' rrv_parse_text: Parse RRV txt files when csv not available
 #'
-#' This function formats and organizes Food View task data from bids/sourcedata into bids/rawdata for a given subject
-#'
+#' This function exports game.csv and summary.csv files from the RRV .txt file. This is for use when game.csv and summary.csv were not automatically generated. CSV files will export into the directory with the .txt file
+#' Function will also return game.csv and summary.csv in dataframes
 #'
 #' @param sub subject label used in sub-label. Leading zeros not required
 #' @param rrv_file string with full path to rrv file
 #' @param overwrite logical indicating if data should be overwritten in /rawdata. Default = FALSE
 #' @param return_data logical indicating if data should be returned. Default = FALSE
-#' @return If return_data is set to TRUE, will return XXX
 #'
 #' @examples
 #'
 #' \dontrun{
 #'
 #' # process task data for the Food View Task
-#' file_name = "~/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/b-childfoodlab_Shared/Active_Studies/MarketingResilienceRO1_8242020/ParticipantData/untouchedRaw/rrv_task/060.txt"
+#' file_name = "/Users/baf44/projects/Keller_Marketing/ParticipantData/untouchedRaw/rrv_task/REACH_060/060.txt"
 #' rrv_csv <- rrv_parse_text(sub = 60, rrv_file = file_name)
 #'
 #' }
@@ -22,18 +21,39 @@
 #'
 #' @export
 
-rrv_parse_text <- function(sub, rrv_file, overwrite = FALSE, return_data = TRUE) {
+rrv_parse_text <- function(sub, rrv_file, overwrite = FALSE) {
 
-  #### create empty dataframe to save data to ####
+  #### IO setup ####
+  if (.Platform$OS.type == "unix") {
+    slash <- '/'
+  } else {
+    slash <- "\\"
+    print('The proc_redcap.R has not been thoroughly tested on Windows systems, may have visit_data_path errors. Contact Bari at baf44@psu.edu if there are errors')
+  }
+
+  #### create empty dataframes to save data to ####
+
+  ## for game.csv
 
   ## created vector with column names
-  columns= c("ID",	"screen",	"reinforcer",	"type",	"session",	"total_time",	"schedule",	"time_block",	"responses",	"reinforcers", 	"total_responses",	"total_reinforcers",	"average_responses",	"average_reinforcers")
+  game_columns= c("ID",	"screen",	"reinforcer",	"type",	"session",	"total_time",	"schedule",	"time_block",	"responses",	"reinforcers", 	"total_responses",	"total_reinforcers",	"average_responses",	"average_reinforcers")
 
   ## pass this vector length to ncol parameter
-  game_df = data.frame(matrix(nrow = 0, ncol = length(columns)))
+  game_df = data.frame(matrix(nrow = 0, ncol = length(game_columns)))
 
   ## assign column names to game_df
-  colnames(game_df) = columns
+  colnames(game_df) = game_columns
+
+  ## for summary.csv
+
+  ## created vector with column names
+  summary_columns= c("ID",	"screen",	"reinforcer",	"type",	"session",	"total_time",	"schedule",	"total_time_blocks",	"total_non_response_time_blocks",	"total_responses",	"total_reinforcers",	"average_responses",	"average_reinforcers")
+
+  ## pass this vector length to ncol parameter
+  summary_df = data.frame(matrix(nrow = 0, ncol = length(summary_columns)))
+
+  ## assign column names to game_df
+  colnames(summary_df) = summary_columns
 
   #### parse text file ####
 
@@ -73,6 +93,9 @@ rrv_parse_text <- function(sub, rrv_file, overwrite = FALSE, return_data = TRUE)
     Goal2 = unlist(strsplit(session_lines[grep("Goal2", session_lines)], "\t"))[3]
     TimeTaken2 = unlist(strsplit(session_lines[grep("Time Taken2", session_lines)], "\t"))[3]
 
+    print(paste0("session ", session_number))
+    print(TimeTaken1)
+    print(TimeTaken2)
     #### Extract Total Responses By Screen ####
 
     total_response_start_line <- grep("Total Responses", session_lines)
@@ -107,10 +130,6 @@ rrv_parse_text <- function(sub, rrv_file, overwrite = FALSE, return_data = TRUE)
     #### Extract Average Responses By Screen ####
 
     timeblock1_start_line <- grep("Time Block : 1$", session_lines) # the $ means end of line, so that Time Block : 10, etc. does not count
-    print("session")
-    print(session_number)
-    print(avg_response_start_line)
-    print(timeblock1_start_line)
     avg_response_lines <- session_lines[(avg_response_start_line+1):(timeblock1_start_line-2)]
 
     # set defaults to NA
@@ -149,6 +168,12 @@ rrv_parse_text <- function(sub, rrv_file, overwrite = FALSE, return_data = TRUE)
     # initialize list to append timeblock lines to
     timeblock_list <- list()
 
+    # set summary values to 0
+    total_timeblocks_screen1 <- 0
+    total_timeblocks_screen2 <- 0
+    total_nonresp_timeblocks_screen1 <- 0
+    total_nonresp_timeblocks_screen2 <- 0
+
     # extract data within timeblocks
     for (timeblock_number in 1:n_timeblocks) {
 
@@ -184,10 +209,26 @@ rrv_parse_text <- function(sub, rrv_file, overwrite = FALSE, return_data = TRUE)
         Screen = parts[[1]][3]
         reinforcer_cat = ifelse(Screen == "1", Goal1, ifelse(Screen == "2", Goal2, NA))
 
-        # create row to append to dataframe
-        data_row <-
+        # Increment summary values
+        if (Screen == 1 ) {
+          total_timeblocks_screen1 = total_timeblocks_screen1 + 1
+
+          if (Responses == 0) {
+            total_nonresp_timeblocks_screen1 = total_nonresp_timeblocks_screen1 + 1
+          }
+
+        } else if (Screen == 2) {
+          total_timeblocks_screen2 = total_timeblocks_screen2 + 1
+
+          if (Responses == 0) {
+            total_nonresp_timeblocks_screen2 = total_nonresp_timeblocks_screen2 + 1
+          }
+        }
+
+        # create row for game.csv
+        game_data_row <-
           data.frame(
-            ID = "sub", #task-level
+            ID = sub, #task-level
             screen = Screen, #reinforcer-level
             reinforcer = reinforcer_cat, #reinforcer-level
             type = ifelse(Type == "Slot Machine Game with Reinforcers", "slot machine", NA), #task-level
@@ -204,11 +245,69 @@ rrv_parse_text <- function(sub, rrv_file, overwrite = FALSE, return_data = TRUE)
           )
 
         # append row to game_df dataframe
-        game_df <- rbind(game_df, data_row)
+        game_df <- rbind(game_df, game_data_row)
 
       }
 
     }
+
+    # create summary dataframe
+    for (screen_number in c("1", "2")) {
+      print(paste0("session ", session_number, "screen ", screen_number))
+
+      # create row for summary.csv
+      summary_data_row <-
+        data.frame(
+          ID = sub, #task-level
+          screen = screen_number, #reinforcer-level
+          reinforcer = ifelse(screen_number == "1", Goal1, ifelse(screen_number == "2", Goal2, NA)), #reinforcer-level
+          type = ifelse(Type == "Slot Machine Game with Reinforcers", "slot machine", NA), #task-level
+          session = session_number, #session-level
+          # total_time = ifelse(screen_number == "1", TimeTaken1, ifelse(screen_number == "2", TimeTaken2, NA)), #session x reinforcer level
+          schedule = ifelse(screen_number == "1", Screen1, ifelse(screen_number == "2", Screen2, NA)), # session level
+          total_time_blocks = ifelse(screen_number == "1", total_timeblocks_screen1, ifelse(screen_number == "2", total_timeblocks_screen2, NA)),
+          total_nonresp_timeblocks = ifelse(screen_number == "1", total_nonresp_timeblocks_screen1, ifelse(screen_number == "2", total_nonresp_timeblocks_screen2, NA)),
+          total_responses = ifelse(screen_number == "1", total_resp_screen1, ifelse(screen_number == "2", total_resp_screen2, NA)), #session x reinforcer level
+          total_reinforcers = ifelse(screen_number == "1", total_reinforcers_screen1, ifelse(screen_number == "2", total_reinforcers_screen2, NA)), #session x reinforcer level
+          average_responses = ifelse(screen_number == "1", avg_resp_screen1, ifelse(screen_number == "2", avg_resp_screen2, NA)), #session x reinforcer level
+          average_reinforcers = ifelse(screen_number == "1", avg_reinforcers_screen1, ifelse(screen_number == "2", avg_reinforcers_screen2, NA)) #session x reinforcer level
+        )
+
+      # append row to game_df dataframe
+      summary_df <- rbind(summary_df, summary_data_row)
+    }
+
   }
 
+
+  ##### Export CSVs ####
+
+  # get data to export into
+  export_dir <- dirname(file_name)
+
+  # set export file paths
+  game_path <- paste0(export_dir, slash, "game.csv")
+  summary_path <- paste0(export_dir, slash, "summary.csv")
+
+  if ( isTRUE(overwrite) | !file.exists(game_path) ) {
+    write.csv(
+      game_df,
+      game_path,
+      quote = FALSE,
+      col.names = TRUE,
+      row.names = FALSE
+    )
+  }
+
+  if ( isTRUE(overwrite) | !file.exists(summary_path) ) {
+    write.csv(
+      summary_df,
+      summary_path,
+      quote = FALSE,
+      col.names = TRUE,
+      row.names = FALSE
+    )
+  }
+  ##### Return dataframes ####
+  return(list(game_df = game_df, summary_df = summary_df))
 }
