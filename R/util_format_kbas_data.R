3f32ead718040b0f517f00b2fc4192a9d9b68863
bfuchs18 <bari.fuchs@gmail.com>
2024-04-25 16:25:24 -0400

    combine version A and version B item and item score columns

--- R/util_format_kbas_data.R
+++ R/util_format_kbas_data.R
@@ -28,6 +28,8 @@ util_format_kbas_data <- function(kbas_data) {
                    lubridate::as_date(kbas_data$kids_brand_awareness_survey_version_b_timestamp)
     )
 
+  #### combine kbas score columns between versions ####
+
   # combine va and vb total scored columns
   kbas_data$kbas_food_score <- ifelse(kbas_data$kbas_version == "A", kbas_data$va_food_score_kbas,
                                       ifelse(kbas_data$kbas_version == "B", kbas_data$vb_food_score_kbas, NA))
@@ -36,11 +38,39 @@ util_format_kbas_data <- function(kbas_data) {
                                      ifelse(kbas_data$kbas_version == "B", kbas_data$vb_toy_score_kbas, NA))
 
   #remove columns
-  kbas_data <- kbas_data[, !grepl('timestamp|va_toy_score|vb_toy_score|va_food_score|vb_food_score', names(kbas_data))]
+  kbas_data <- kbas_data[, !grepl('timestamp|va_toy_score|vb_toy_score|va_food_score|vb_food_score|va_q16_score_2', names(kbas_data))]
+
+  #### combine q_*score columns between versions ####
+
+  # define ways to re-name score columns
+  q_numbers <- seq(1, 25)
+  food_qs <- paste0("food_", q_numbers, "_score")
+  toy_qs <- paste0("toy_", q_numbers, "_score")
+
+  new_col_va <- c(food_qs, toy_qs) #version a order: food then toys
+  new_col_vb <- c(toy_qs, food_qs) #version b order: toys then food
+
+  # process version A data
+  kbas_va_data <- kbas_data[kbas_data$kbas_version == "A" & !is.na(kbas_data$kbas_version) ,] #subset verion A data
+  kbas_va_data <- kbas_va_data[, -grep("^vb_", colnames(kbas_va_data))] # remove version B columns (start with "vb_")
+  names(kbas_va_data) <- gsub("va_", "", names(kbas_va_data)) # remove "va_" from column names
+  q_cols_va <- names(kbas_va_data)[grep("^q.*score$", names(kbas_va_data))] # get column names that start with q and end with score
+  kbas_va_data <- kbas_va_data %>% dplyr::rename_with(~new_col_va, .cols = all_of(q_cols_va)) #rename columns
+
+  # process version B data
+  kbas_ba_data <- kbas_data[kbas_data$kbas_version == "B" & !is.na(kbas_data$kbas_version) ,] #subset verion B data
+  kbas_ba_data <- kbas_ba_data[, -grep("^va_", colnames(kbas_ba_data))] # remove version A columns (start with "va_")
+  names(kbas_ba_data) <- gsub("vb_", "", names(kbas_ba_data)) # remove "vb_" from column names
+  q_cols_vb <- names(kbas_ba_data)[grep("^q.*score$", names(kbas_ba_data))] # get column names that start with q and end with score
+  kbas_ba_data <- kbas_ba_data %>% dplyr::rename_with(~new_col_vb, .cols = all_of(q_cols_vb)) #rename columns
+
+  # combine versions A and B with new columns names
+  kbas_data <- rbind(kbas_va_data, kbas_ba_data)
 
   #reorder columns
   kbas_data <-
     kbas_data %>% dplyr::relocate("session_id", .after = 1) %>% dplyr::relocate(dplyr::contains("form_date"), .after = 2) %>% dplyr::relocate(dplyr::contains("kbas_version"), .after = 3) %>% dplyr::relocate(dplyr::contains("kbas_food_score"), .after = 4)  %>% dplyr::relocate(dplyr::contains("kbas_toy_score"), .after = 5)
+
 
 
   # return data
