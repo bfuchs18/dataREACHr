1de6a8bdb3961336bd830d248203bc615c1b8baa
bfuchs18 <bari.fuchs@gmail.com>
2024-05-01 12:21:30 -0400

    add MeasurementToolMetadata

--- R/json_infancy.R
+++ R/json_infancy.R
@@ -10,6 +10,8 @@
 json_infancy <- function() {
 
   infancy_list <- list(
+    'MeasurementToolMetadata' = list(
+      Description = 'Questions related to the child\'s birth and infancy that parents answered as part of the REDCap form visit_1_demographics'),
     participant_id = list( Description = 'participant id number'),
     session_id = list( Description = 'BIDS session ID indicating when data was collected',
                        Levels = list ('ses-1' = 'session 1 / baseline')),
@@ -24,7 +26,7 @@ json_infancy <- function() {
     demo_exclusive_feeding = list( Description = 'What was the total duration, in months, that your child was exclusively (only) breastfed? Please enter the duration, in months, that your child was only fed breastmilk and no formula or solid food was consumed. '),
     demo_tot_breastfeeding = list( Description = 'What was the total duration, in months, that your child was breastfed? Please enter the duration, in months, that your child was fed both breast milk and formula and/or solid food. '),
     demo_solid_food = list( Description = 'At what age, in months, was your child first introduced to solid food? '),
-    birthweight_ounces_total = list( Description = 'Chid birthweight computed from demo_birthweight_pounds_component and demo_birthweight_ounces_component (16*demo_birthweight_pounds_component + demo_birthweight_ounces_component)',
+    birthweight_ounces_total = list( Description = 'Child birthweight computed from demo_birthweight_pounds_component and demo_birthweight_ounces_component (16*demo_birthweight_pounds_component + demo_birthweight_ounces_component)',
                                      Derivative = TRUE,
                                      Unit = "ounces")
     )
