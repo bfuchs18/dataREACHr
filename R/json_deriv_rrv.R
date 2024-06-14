edef4c16fb2f6da9bfeebb947c24e7a839b0bbcb
bfuchs18 <bari.fuchs@gmail.com>
2024-06-14 13:47:10 -0400

    add long database variables; return 2 separate strings with meta-data for overall summary and long summary databases

--- R/json_deriv_rrv.R
+++ R/json_deriv_rrv.R
@@ -1,15 +1,15 @@
-#' json_deriv_rrv: Generates a json file for the derivative RRV data
+#' json_deriv_rrv: Generates 2 json file for the derivative RRV data
 #'
-#' This function generates a json file the summary RRV outcomes reported in derivatives/beh_databases
+#' This function generates 2 json files RRV databases reported in derivatives/beh_databases: (1) for total summary outcomes, (2) for session summary outcomes (long data)
 #'
-#' @return A string with data stored in JSON format containing meta-data the summary RRV database
+#' @return A list of 2 strings (labels: rrv_summary_json, rrv_summary_long_json) with data stored in JSON format containing meta-data the summary RRV database
 #'
 #'
 #' @export
 
 json_deriv_rrv <- function() {
 
-  rrv_list <- list(
+  rrv_summary_list <- list(
     'MeasurementToolMetadata' = list(
       Description = '' ,
       Reference = '',
@@ -77,14 +77,44 @@ json_deriv_rrv <- function() {
                                     Derivative = TRUE)
     )
 
+
+  rrv_summary_long_list <- list(
+    'MeasurementToolMetadata' = list(
+      Description = '',
+      Reference = '',
+      TermURL = ''),
+    participant_id = list( Description = 'participant id number'),
+    reinforcer = list( Description = 'Reinforcer',
+                       Levels = list ('Candy' = 'candy',
+                                      'Toy' = 'toy')),
+    session = list( Description = 'RRV task session number. Each session corresponds to a different reinforcement schedule'),
+    session_time = list( Description = 'Time spent within a session for a given reinforcer/screen',
+                         Unit = "seconds"),
+    schedule = list( Description = 'Reinforcement schedule',
+                     Levels = list ('Variable Response Based 1:4' = '1 reinforcer earned per 4 responses',
+                                    'Variable Response Based 1:8' = '1 reinforcer earned per 8 responses')),
+    session_responses = list( Description = 'Number of responses made within a session for a given reinforcer'),
+    session_reinforcers = list( Description = 'Number of reinforcers earned within a session for a given reinforcer'),
+    session_blocks = list( Description = 'Number of blocks within a session for a given reinforcer'),
+    session_nonresp_blocks = list( Description = 'Number of blocks with 0 responses within a session for a given reinforcer'),
+    session_average_responses = list( Description = '??'),
+    session_average_reinforcers = list( Description = '??')
+  )
+
   # convert formatting to JSON
-  rrv_json <- RJSONIO::toJSON(rrv_list, pretty = TRUE)
+  rrv_summary_json <- RJSONIO::toJSON(rrv_summary_list, pretty = TRUE)
+  rrv_summary_long_json <- RJSONIO::toJSON(rrv_summary_long_list, pretty = TRUE)
 
   # double check
-  if (isFALSE(RJSONIO::isValidJSON(rrv_json, asText = TRUE))){
-    print('RRV derivative JSON file may be invalid')
+  if (isFALSE(RJSONIO::isValidJSON(rrv_summary_json, asText = TRUE))){
+    print('RRV summary derivative JSON file may be invalid')
   }
 
-  return(rrv_json)
+  if (isFALSE(RJSONIO::isValidJSON(rrv_summary_long_json, asText = TRUE))){
+    print('RRV long summary derivative JSON file may be invalid')
+  }
+
+  return(list(rrv_summary_json = rrv_summary_json,
+              rrv_summary_long_json = rrv_summary_long_json))
 
 }
