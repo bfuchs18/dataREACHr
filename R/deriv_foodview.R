28f587f73a66f3429fa1f0f66976ce15813b97e1
Bari Fuchs <bari.fuchs@gmail.com>
2024-07-25 12:08:04 -0400

    init commit

---
+++ R/deriv_foodview.R
@@ -0,0 +1,122 @@
+#' deriv_foodview: Generate derivative databases for Foodview Task analyses
+#'
+#' This function generates FoodView derivative databases from participant-level FoodView files
+#'
+#' @param data a list of list of dataframes. The top-level list represents individual subjects, and each subject has a sublist of dataframes. Each dataframe contains events data for a specific run of the foodview task for a given sub. A suitable list is returned by proc_task, or can be gathered from files in bids/rawdata
+#' @param return_data If return_data is set to TRUE, will return a list including:
+#'  1) a dataframe with long summary data by commerical_condition
+#'
+#' @examples
+#'
+#' \dontrun{
+#'
+#' # process task data
+#' base_dir = "/Users/bari/Library/CloudStorage/OneDrive-ThePennsylvaniaStateUniversity/b-childfoodlab_Shared/Active_Studies/MarketingResilienceRO1_8242020/ParticipantData/"
+#' task_data <- proc_task(base_wd = base_dir, return_data = TRUE)
+#'
+#' # get deriv foodview data from processed task data
+#' food_view_summary <- deriv_foodview(task_data$foodview)
+#' }
+
+deriv_foodview <- function(data) {
+
+  # create output dataframes
+  summary_df <- data.frame()
+
+  for (i in 1:length(data)) {
+
+    # extract subjects foodview data
+    foodview_data <- data[[i]]
+
+    # determine number of runs
+    n_runs <- length(foodview_data)
+
+    # make one dataframe with all run data
+    combined_df <- dplyr::bind_rows(foodview_data)
+
+    # sub
+    sub = combined_df$sub[1]
+
+    # subset image rows
+    jpeg_rows <- combined_df[grep("jpeg", combined_df$stim_file_name),]
+
+    for (cond in c("food", "toy")) {
+
+      # subset images in condition
+      cond_jpeg_rows <- jpeg_rows[jpeg_rows$commercial_cond == cond,]
+
+      n_image = nrow(cond_jpeg_rows)
+      p_resp = nrow(cond_jpeg_rows[cond_jpeg_rows$response == 1 | cond_jpeg_rows$response == 2,]) / n_image
+      p_want_of_resp = nrow(cond_jpeg_rows[cond_jpeg_rows$response == 1,]) / nrow(cond_jpeg_rows[cond_jpeg_rows$response == 1 | cond_jpeg_rows$response == 2,])
+      avg_rt = mean(as.numeric(cond_jpeg_rows[cond_jpeg_rows$response == 1 | cond_jpeg_rows$response == 2,]$response_time))
+
+      hed_rows <- cond_jpeg_rows[cond_jpeg_rows$food_ed == "high",]
+      hed_n_image = nrow(hed_rows)
+      hed_p_resp = nrow(hed_rows[hed_rows$response == 1 | hed_rows$response == 2,]) / hed_n_image
+      hed_p_want_of_resp = nrow(hed_rows[hed_rows$response == 1,]) / nrow(hed_rows[hed_rows$response == 1 | hed_rows$response == 2,])
+      hed_avg_rt = mean(as.numeric(hed_rows[hed_rows$response == 1 | hed_rows$response == 2,]$response_time))
+
+      led_rows <- cond_jpeg_rows[cond_jpeg_rows$food_ed == "low",]
+      led_n_image = nrow(led_rows)
+      led_p_resp = nrow(led_rows[led_rows$response == 1 | led_rows$response == 2,]) / led_n_image
+      led_p_want_of_resp = nrow(led_rows[led_rows$response == 1,]) / nrow(led_rows[led_rows$response == 1 | led_rows$response == 2,])
+      led_avg_rt = mean(as.numeric(led_rows[led_rows$response == 1 | led_rows$response == 2,]$response_time))
+
+      sweet_rows <- cond_jpeg_rows[cond_jpeg_rows$food_taste == "sweet",]
+      sweet_n_image = nrow(sweet_rows)
+      sweet_p_resp = nrow(sweet_rows[sweet_rows$response == 1 | sweet_rows$response == 2,]) / sweet_n_image
+      sweet_p_want_of_resp = nrow(sweet_rows[sweet_rows$response == 1,]) / nrow(sweet_rows[sweet_rows$response == 1 | sweet_rows$response == 2,])
+      sweet_avg_rt = mean(as.numeric(sweet_rows[sweet_rows$response == 1 | sweet_rows$response == 2,]$response_time))
+
+      savory_rows <- cond_jpeg_rows[cond_jpeg_rows$food_taste == "savory",]
+      savory_n_image = nrow(savory_rows)
+      savory_p_resp = nrow(savory_rows[savory_rows$response == 1 | savory_rows$response == 2,]) / savory_n_image
+      savory_p_want_of_resp = nrow(savory_rows[savory_rows$response == 1,]) / nrow(savory_rows[savory_rows$response == 1 | savory_rows$response == 2,])
+      savory_avg_rt = mean(as.numeric(savory_rows[savory_rows$response == 1 | savory_rows$response == 2,]$response_time))
+
+      # summarize data across task for condition
+      cond_summary_row <-
+        data.frame(
+          participant_id = sub,
+          commerical_cond = cond,
+
+          n_image = n_image,
+          p_resp = p_resp,
+          p_want_of_resp = p_want_of_resp,
+          avg_rt = avg_rt,
+
+          hed_n_image = hed_n_image,
+          hed_p_resp = hed_p_resp,
+          hed_p_want_of_resp = hed_p_want_of_resp,
+          hed_avg_rt = hed_avg_rt,
+
+          led_n_image = led_n_image,
+          led_p_resp = led_p_resp,
+          led_p_want_of_resp = led_p_want_of_resp,
+          led_avg_rt = led_avg_rt,
+
+          sweet_n_image = sweet_n_image,
+          sweet_p_resp = sweet_p_resp,
+          sweet_p_want_of_resp = sweet_p_want_of_resp,
+          sweet_avg_rt = sweet_avg_rt,
+
+          savory_n_image = savory_n_image,
+          savory_p_resp = savory_p_resp,
+          savory_p_want_of_resp = savory_p_want_of_resp,
+          savory_avg_rt = savory_avg_rt
+        )
+
+
+      # add row to dataframe
+      if (nrow(summary_df) == 0) {
+        summary_df <- cond_summary_row
+      } else {
+        summary_df <- dplyr::bind_rows(summary_df, cond_summary_row)
+      }
+    }
+
+  }
+
+  return(list(summary_by_cond = summary_df))
+}
+
